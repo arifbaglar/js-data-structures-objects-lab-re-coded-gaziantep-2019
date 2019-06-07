ec578f5af5b59be54d54b9fd2e51af9f8d3db05b
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-06-07 07:21:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution in this file!
-const driver={name:'Sam'};
+let driver={name:'Sam'};
 function updateDriverWithKeyAndValue(driver, key, value){
   const newdriver={}
   newdriver[key] = value;
