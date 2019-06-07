557615e14b4c939e77a86b9554474aed1841e0e6
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-06-07 06:05:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution in this file!
-const driver={name: 'Sam',address: '11 Broadway'};
+const driver={name: 'Sam'};
 function updateDriverWithKeyAndValue(driver, key, value){
-  
+  const newdriver={}
 }
